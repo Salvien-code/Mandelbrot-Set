8a72853ee25203eb456035777251497ed8537bab
Simon Samuel <salviensky@gmail.com>
2022-11-04 16:37:41 +0100

    Added mandelbrot_at_point() to main.rs

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -20,9 +20,22 @@ fn calculate_mandelbrot(
             let escaped_at = mandelbrot_at_point(cx, cy, max_iters);
             row.push(escaped_at);
         }
-        all_rows.push(row)
+        rows.push(row)
     }
     rows
+}
+
+fn mandelbrot_at_point(cx: f64, cy: f64, max_iters: usize) -> usize {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    let c = Complex::new(cx, cy);
+
+    for i in 0..max_iters {
+        if z.norm() > 2.0 {
+            return i;
+        }
+        z = z * z + c;
+    }
+    max_iters
 }
 
 fn main() {
